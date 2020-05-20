9a390ce157abc7aab49d5f64c832f0efdf4f5c64
Clemens Schmid <schmid@shh.mpg.de>
2020-05-20 17:44:23 +0200

    modified merge functions to make plink script and everything else work nicely together

--- poseidon2_merge.sh
+++ poseidon2_merge.sh
@@ -7,15 +7,19 @@ _merge() {
   _input_file_with_list_of_poseidon_modules=${1}
   _output_directory=${2}
   # prepare other variables
-  _plink_input_file="/tmp/poseidon2_merge_plink_input_file"
-  _plink_order_file="/tmp/poseidon2_merge_plink_order_file"
+  _output_files_name="poseidon2_merge_$(date +'%Y_%m_%d')"
+  _plink_input_file="${2}/poseidon2_merge_plink_input_file"
+  _plink_order_file="${2}/poseidon2_merge_plink_order_file"
   # make output directory
   mkdir -p ${_output_directory}
   # run steps
   _create_binary_file_list_file ${_input_file_with_list_of_poseidon_modules} ${_plink_input_file}
   _create_order_file_from_fam_files ${_input_file_with_list_of_poseidon_modules} ${_plink_order_file}
-  #_plink_merge ${_plink_input_file} ${_order_file} ${_output_directory}
-  _janno_merge ${_input_file_with_list_of_poseidon_modules} ${_output_directory}
+  _janno_merge ${_input_file_with_list_of_poseidon_modules} ${_output_directory} ${_output_files_name}
+  _plink_merge ${_plink_input_file} ${_plink_order_file} ${_output_directory} ${_output_files_name}
+  # delete temporary files
+  #rm "${2}/poseidon2_merge_plink_input_file"
+  #rm "${2}/poseidon2_merge_plink_order_file"
 }
 
 _create_binary_file_list_file() {
@@ -52,11 +56,11 @@ _plink_merge() {
   # start message
   printf "Merge genome data with plink...\\n"
 
-  sbatch -p "short" -c 4 --mem=10000 -J "plink-merge" --wrap="plink --merge-list ${1} --make-bed --indiv-sort f ${2} --out ${3}_TF"
-  # write slurm logs somewhere
+  # TODO: write slurm logs somewhere
+  sbatch -p "short" -c 4 --mem=10000 -J "poseidon2_merge_plink" -o "test_log/poseidon2_%j.out" -e "test_log/poseidon2_%j.err" --wrap="cat ${1} && plink --merge-list ${1} --make-bed --indiv-sort f ${2} --out ${3}/${4}"
 
-  sbatch -p "short" -c 1 --mem=10000 -J "extract_SNPs" --wrap="plink --bfile ${3}_TF --extract ${4} --make-bed --out ${5}_HO"
   # To extract Human Origins SNPs for PCA & other analysis with modern samples
+  #sbatch -p "short" -c 1 --mem=10000 -J "extract_SNPs" --wrap="plink --bfile ${3}_TF --extract ${4} --make-bed --out ${5}_HO"
 
   # end message
   printf "Done\\n"
@@ -74,7 +78,7 @@ _janno_merge() {
   # start message
   printf "Merge janno files...\\n"
   _input_file=${1}
-  _output_file="${2}/test_merged_janno.janno"
+  _output_file="${2}/${3}.janno"
   # loop through all modules directories
   _janno_files=()
   while read p; do
