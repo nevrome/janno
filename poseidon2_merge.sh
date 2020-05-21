b97eff24b72de959f8fce26089675066add73597
Clemens Schmid <schmid@shh.mpg.de>
2020-05-21 12:57:04 +0200

    further work on the merge script console output

--- poseidon2_merge.sh
+++ poseidon2_merge.sh
@@ -1,19 +1,18 @@
 # Author: Clemens & Ayshin
 
-#### Main function ####
-
 _merge() {
   # catch input variables
   _input_file_with_list_of_poseidon_modules=${1}
   _output_directory=${2}
   # prepare other variables
-  _current_date="$(date +'%Y_%m_%d')"
+  _current_date="$(date +'%Y_%m_%d_%H_%M')"
   _output_files_name="poseidon2_merge_${_current_date}"
   _log_file_directory="poseidon2_log/${_current_date}"
   _plink_input_file="${2}/poseidon2_merge_plink_input_file.txt"
   _plink_order_file="${2}/poseidon2_merge_plink_order_file.txt"
   # start message
   _merge_start_message ${_input_file_with_list_of_poseidon_modules} ${_output_directory} ${_output_files_name} ${_log_file_directory}
+  _print_modules ${_input_file_with_list_of_poseidon_modules}
   # make output and log directory
   mkdir -p ${_output_directory}
   mkdir -p ${_log_file_directory}
@@ -22,15 +21,17 @@ _merge() {
   _janno_merge ${_input_file_with_list_of_poseidon_modules} ${_output_directory} ${_output_files_name}
   _create_order_file_from_fam_files ${_input_file_with_list_of_poseidon_modules} ${_plink_order_file}
   _plink_merge ${_plink_input_file} ${_plink_order_file} ${_output_directory} ${_output_files_name} ${_log_file_directory}
+  
+  printf "\\n"
 }
 
 _merge_start_message() {
 cat << EOF
                        _     _             ____  
-  _ __   ___  ___  ___(_) __| | ___  _ __ |___ \ 
- | '_ \ / _ \/ __|/ _ \ |/ _  |/ _ \| '_ \  __) |
+  ____   ___  ___  ___(_) __| | ___  ____ |___ \ 
+ |  _ \ / _ \/ __|/ _ \ |/ _  |/ _ \|  _ \  __) |
  | |_) | (_) \__ \  __/ | (_| | (_) | | | |/ __/ 
- | .__/ \___/|___/\___|_|\__,_|\___/|_| |_|_____|
+ |  __/ \___/|___/\___|_|\____|\___/|_| |_|_____|
  |_| 
 
 merge => Merges multiple poseidon directories
@@ -43,8 +44,22 @@ Log file directory:		${4}
 EOF
 }
 
+_print_modules() {
+  printf "Modules to be merged:\\n"
+  _input_file=${1}
+  # loop through all modules directories
+  while read p; do
+    # ignore empty names (empty lines in the input dir list)
+    if [ -z "${p}" ]
+    then
+      continue
+    fi
+    printf "=> ${p}\\n"
+  done <${_input_file}
+  printf "\\n"
+}
+
 _create_binary_file_list_file() {
-  # start message
   printf "Creating input file for plink merge...\\n"
   # input file
   _input_file=${1}
@@ -82,7 +97,6 @@ _merge_multiple_files_with_header() {
 }
 
 _janno_merge() {
-  # start message
   printf "Merge janno files...\\n"
   _input_file=${1}
   _output_file="${2}/${3}.janno"
@@ -115,7 +129,6 @@ _merge_multiple_files_and_cut_first_two_columns() {
 }
 
 _create_order_file_from_fam_files() {
-  # start message
   printf "Merge fam files to get order file...\\n"
   _input_file=${1}
   _output_file=${2}
@@ -140,13 +153,7 @@ _create_order_file_from_fam_files() {
 }
 
 _plink_merge() {
-  # start message
   printf "Merge genome data with plink...\\n=> "
-
-  # TODO: write slurm logs somewhere
   sbatch -p "short" -c 4 --mem=10000 -J "poseidon2_merge_plink" -o "${5}/poseidon2_%j.out" -e "${5}/poseidon2_%j.err" --wrap="plink --merge-list ${1} --make-bed --indiv-sort f ${2} --out ${3}/${4}"
-
-  # To extract Human Origins SNPs for PCA & other analysis with modern samples
-  #sbatch -p "short" -c 1 --mem=10000 -J "extract_SNPs" --wrap="plink --bfile ${3}_TF --extract ${4} --make-bed --out ${5}_HO"
 }
 
